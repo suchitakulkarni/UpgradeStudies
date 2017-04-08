ecf94f6d5e927ad99bed27cd77e3d3815ba74310
gzevi <giovanni.zevidellaporta@gmail.com>
2017-04-08 22:42:18 +0200

    also runs on signal, and automatic hadd of histos

--- skim/do_skim.sh
+++ skim/do_skim.sh
@@ -10,18 +10,20 @@ mkdir -p ${OUTPATH}
 
 # Basil's skims
 #INPATH=root://eoscms.cern.ch//eos/cms/store/user/bschneid/SUSY_Upgrade_Skimmer/output12_BB
-#declare -a Samples=(qcd_ht ttsl ttdl singletop ttw_mg_lo ttz_mg_lo ttg dyjetsll_ht wjets_ht gjets_dr0p05_ht zinv_ht)
 #declare -a Samples=(BB0_ntuple)
 
-
 # Central samples
-INPATH=root://eoscms.cern.ch//eos/cms/store/group/upgrade/delphes_output/DelphesFromLHE_333pre16_2016Aug/
-
+#INPATH=root://eoscms.cern.ch//eos/cms/store/group/upgrade/delphes_output/DelphesFromLHE_333pre16_2016Aug/
 #declare -a Samples=(BB-4p-0-300-v1510_14TEV_200PU)
-declare -a Samples=(BB-4p-0-300-v1510_14TEV_200PU BB-4p-1300-2100-v1510_14TEV_200PU BB-4p-2100-100000-v1510_14TEV_200PU BB-4p-300-700-v1510_14TEV_200PU BB-4p-700-1300-v1510_14TEV_200PU)
-#declare -a Samples=(BBB-4p-0-600-v1510_14TEV_200PU BBB-4p-1300-100000-v1510_14TEV_200PU BBB-4p-600-1300-v1510_14TEV_200PU)
+#declare -a Samples=(BB-4p-0-300-v1510_14TEV_200PU BB-4p-1300-2100-v1510_14TEV_200PU BB-4p-2100-100000-v1510_14TEV_200PU BB-4p-300-700-v1510_14TEV_200PU BB-4p-700-1300-v1510_14TEV_200PU)
 
-## Submit without using wildcard: figure out names of files in each directory, submit one job per file
+# Suchita's C2N4
+INPATH=root://eoscms.cern.ch//eos/cms/store/cmst3/group/susy/upgrade/C2N4_delphised/
+declare -a Samples=(delphised_substructure_200PU) # not a very descriptive name unfortunately
+
+
+## Submit without using wildcard: "Samples" contains folder names, but then
+## we figure out filenames in each folder and submit one job per file
 for SAMPLE in ${Samples[@]}
   do
     echo Loopling over ${INPATH}/${SAMPLE}
@@ -33,7 +35,7 @@ for SAMPLE in ${Samples[@]}
     done
 done
 
-## Submit with wildcards
+## Submit with wildcards. IF eos is mounted, then this work and can submit one job per folder
 #for SAMPLE in ${Samples[@]}
 ##  do root -b -l -q skim.C\(\"$INPATH\",\"$OUTPATH\",\"$SAMPLE\"\) >& ${LOGDIR}/log_${SAMPLE}.txt &
 #  do root -b -l -q skimslim.C\(\"$INPATH\",\"$OUTPATH\",\"$SAMPLE\"\) >& ${LOGDIR}/log_${SAMPLE}.txt &
