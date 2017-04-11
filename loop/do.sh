6b66bd11961d54f58a4df62ea0ef73186082a542
gzevi <giovanni.zevidellaporta@gmail.com>
2017-04-11 22:05:52 +0200

    loop on signal too

--- loop/do.sh
+++ loop/do.sh
@@ -8,17 +8,29 @@ mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
 
 #most recent MC
-#INDIR=/home/gzevi/UpgradeSkims/
-INDIR=~gzevi/public/UpgradeSkims/
+INDIR=/home/gzevi/UpgradeSkims11Apr17/
+#INDIR=~gzevi/public/UpgradeSkims/
 #declare -a Samples=(BB-4p-300-700)
-declare -a Samples=(BB-4p-0-300 BB-4p-300-700 BB-4p-700-1300 BB-4p-1300-2100 BB-4p-2100-100000 BBB-4p-0-600 BBB-4p-600-1300 BBB-4p-1300-100000)
+declare -a Samples=(BB-4p-0-300 BB-4p-300-700 BB-4p-700-1300 BB-4p-1300-2100 BB-4p-2100-100000 BBB-4p-0-600 BBB-4p-600-1300 BBB-4p-1300-100000 )
 
 for SAMPLE in ${Samples[@]}; do
 
-    # Get the xsec (in pb) and nevts   <-- Doesn't work for signal yet. Need a hack.
+    # Get the xsec (in fb) and nevts   <-- Doesn't work for signal yet. Hack below
     # weight = (xsect*lumi)/nevents
-    XSEC=`grep ${SAMPLE} configSkimmer.txt | awk '{print $6*1}'`
+    XSEC=`grep ${SAMPLE} configSkimmer.txt | awk '{print $6*1}'` # [fb]
     NEVTS=`grep ${SAMPLE} configSkimmer.txt | awk '{print $7*1}'`
+    echo $XSEC, $NEVTS
+
+    echo nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} ${XSEC} ${NEVTS}
+    eval "nohup ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} ${XSEC} ${NEVTS} >& ${LOGDIR}/log_${SAMPLE}.txt &"
+done
+
+declare -a Samples=(rns_c2n4_decayedALL)
+
+for SAMPLE in ${Samples[@]}; do
+
+    XSEC=250.72 # from Suchita's email [fb]
+    NEVTS=30000
     echo $XSEC, $NEVTS
 
     echo nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} ${XSEC} ${NEVTS}
